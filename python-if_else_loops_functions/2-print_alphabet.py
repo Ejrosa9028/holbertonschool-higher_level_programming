a5de8f58ffa687d2edd1b4af69db550c706c5c2b
Ejrosa9028 <emanueljavierrosa@gmail.com>
2025-01-19 20:41:31 -0400

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game #2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 
-for i in range(97, 123):  # Los valores ASCII de 'a' (97) a 'z' (122)
-    print(chr(i), end="")  # Usamos chr() para convertir el valor ASCII a carácter
+print("".join("{:c}".format(i) for i in range(97, 123)), end="")

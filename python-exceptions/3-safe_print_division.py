c4ac6317fd2ceeda09c58676c179a8c5a08de94e
Ejrosa9028 <emanueljavierrosa@gmail.com>
2025-01-26 21:42:30 -0400

    3. Integers division with debug #2

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         result = a / b  # Intentamos dividir a entre b
     except ZeroDivisionError:
-        result = None  # Si ocurre una excepción de división por cero, asignamos None
+        result = None
     finally:
-        print("Inside result: {}".format(result))  # Siempre imprimimos el resultado dentro del finally
-        return result  # Devolvemos el resultado (o None si hubo un error)
+        print("Inside result: {}".format(result))  #imprimimos el resultado 
+        return result  # Devolvemos el resultado

8cd095e22be1fdcf993a84983a924e282c44a316
Ejrosa9028 <emanueljavierrosa@gmail.com>
2025-02-09 22:19:14 -0400

    1. My list #2

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""
+Este módulo contiene la clase MyList, que hereda de la clase list de Python.
+
+La clase MyList tiene un método llamado print_sorted, que imprime la lista
+ordenada de manera ascendente sin modificar la lista original.
+
+Métodos:
+    - print_sorted: Imprime la lista ordenada de menor a mayor sin modificarla.
+"""
+
+
 class MyList(list):
     """
     Clase MyList que hereda de list y tiene un método print_sorted

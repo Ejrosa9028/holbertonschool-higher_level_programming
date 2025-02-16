ea8dba1b20807766ffccf29bed1211ebb65117d5
Ejrosa9028 <emanueljavierrosa@gmail.com>
2025-02-16 01:02:29 -0400

    1. Write to a file #2

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+"""
+Módulo que contiene la función write_file para escribir texto en un archivo.
 
-# Escribe un texto en un archivo y devuelve el número de caracteres escritos.
+Este módulo define una función que permite escribir una cadena de texto
+en un archivo codificado en UTF-8 y devuelve cantidad de caracteres escritos.
+"""
+
 
 def write_file(filename="", text=""):
     """
     Escribe una cadena de texto en un archivo (UTF-8) y devuelve la cantidad
     de caracteres escritos.
+
+    Si el archivo ya existe, su contenido se sobreescribirá.
+    Si el archivo no existe, se creará uno nuevo.
 
     Args:
         filename (str): Nombre del archivo donde se guardará el texto.

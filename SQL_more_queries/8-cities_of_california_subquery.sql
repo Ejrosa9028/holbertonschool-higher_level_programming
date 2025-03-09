3105db2f59726ffd75fdcc6de34d31bb8c04480d
Ejrosa9028 <132318096+Ejrosa9028@users.noreply.github.com>
2025-03-09 06:18:45 -0400

    8. Cities of California

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,11 @@
+-- Selecciona ciudades de California
+SELECT id, name
+FROM cities
+WHERE
+    state_id = (
+        SELECT id
+        FROM states
+        WHERE
+            name = 'California'
+    )
+ORDER BY id ASC;

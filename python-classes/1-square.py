54d0009a72c575f54b16ddb9605ec7c67004a697
Ejrosa9028 <132318096+Ejrosa9028@users.noreply.github.com>
2025-05-25 22:03:12 -0400

    1. Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+"""Este módulo define una clase Square."""
+
+
+class Square:
+    """Representa un cuadrado con un atributo de tamaño privado."""
+
+    def __init__(self, size):
+        """Inicializar un nuevo cuadrado con un tamaño determinado."""
+        self.__size = size
